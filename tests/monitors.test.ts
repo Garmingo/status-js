0203f4563aef279e1a7e6611a47a84d93ad76b7c
Alexander Neitzel <alexander.neitzel@garmingo.com>
2024-11-02 12:49:43 +0100

    Add tests for retrieving monitors with validation checks

--- tests/monitors.test.ts
+++ tests/monitors.test.ts
@@ -5,6 +5,74 @@
  */
 
 import { StatusAPI } from "../src";
+import { ERROR_CODE } from "../src/error";
+
+const statusAPI = new StatusAPI(process.env.API_KEY as string);
+
+describe("Retrieve Monitors List", () => {
+  test("Get a list of monitors", async () => {
+    const result = await statusAPI.monitors.getAll();
+
+    expect(result.success).toBe(true);
+  });
+
+  test("Fail when limit is not a number", async () => {
+    const result = await statusAPI.monitors.getAll("test" as unknown as number);
+
+    expect(result.success).toBe(false);
+
+    if (result.success) {
+      return;
+    }
+    expect(result.errorCode).toBe(ERROR_CODE.BAD_REQUEST);
+  });
+
+  test("Fail when page is not a number", async () => {
+    const result = await statusAPI.monitors.getAll(
+      undefined,
+      "test" as unknown as number
+    );
+
+    expect(result.success).toBe(false);
+
+    if (result.success) {
+      return;
+    }
+    expect(result.errorCode).toBe(ERROR_CODE.BAD_REQUEST);
+  });
+
+  test("Fail when limit is less than 1", async () => {
+    const result = await statusAPI.monitors.getAll(-1);
+
+    expect(result.success).toBe(false);
+
+    if (result.success) {
+      return;
+    }
+    expect(result.errorCode).toBe(ERROR_CODE.BAD_REQUEST);
+  });
+
+  test("Fail when page is less than 1", async () => {
+    const result = await statusAPI.monitors.getAll(undefined, -1);
+
+    expect(result.success).toBe(false);
+
+    if (result.success) {
+      return;
+    }
+    expect(result.errorCode).toBe(ERROR_CODE.BAD_REQUEST);
+  });
+
+  test("Result is empty when page is too high", async () => {
+    const result = await statusAPI.monitors.getAll(undefined, 1000000);
+
+    expect(result.success).toBe(true);
+    if (!result.success) {
+      return;
+    }
+    expect(result.data.monitors).toHaveLength(0);
+  });
+});
 
 test("Get a list of monitors", async () => {
   const statusAPI = new StatusAPI(process.env.API_KEY as string);
