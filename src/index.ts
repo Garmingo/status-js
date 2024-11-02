e4442d2f81c471af11eea7d879a5a9523a07b367
Alexander Neitzel <alexander.neitzel@garmingo.com>
2024-11-02 01:40:01 +0100

    Restructured the setup again

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@
  *   Unauthorized use, reproduction, and distribution of this source code is strictly prohibited.
  */
 
-import monitors from "./monitor";
+import { Monitor, MonitorCreate, MonitorUpdate } from "./monitor";
 
 export class StatusAPI {
   private readonly apiKey: string;
@@ -16,7 +16,144 @@ export class StatusAPI {
   /**
    * All CRUD Functions that can be performed on Monitors.
    */
-  public monitors = monitors;
+  public monitors = {
+    /**
+     * Get all Monitors.
+     */
+    getAll: async (
+      limit?: number,
+      page?: number
+    ): Promise<
+      | {
+          success: true;
+          data: Monitor[];
+        }
+      | {
+          success: false;
+          message: string;
+        }
+    > => {
+      return {
+        success: false,
+        message: "Not implemented",
+      };
+    },
+
+    /**
+     * Get a Monitor by its ID.
+     * @param id - ID of the Monitor.
+     */
+    get: async (
+      id: string
+    ): Promise<
+      | {
+          success: true;
+          data: Monitor;
+        }
+      | {
+          success: false;
+          message: string;
+        }
+    > => {
+      return {
+        success: false,
+        message: "Not implemented",
+      };
+    },
+
+    /**
+     * Get all Events for a monitor.
+     * @param id - ID of the Monitor.
+     */
+    getEvents: async (
+      id: string,
+      limit?: number,
+      page?: number
+    ): Promise<
+      | {
+          success: true;
+          data: Event[];
+        }
+      | {
+          success: false;
+          message: string;
+        }
+    > => {
+      return {
+        success: false,
+        message: "Not implemented",
+      };
+    },
+
+    /**
+     * Create a new Monitor.
+     * @param monitor - Monitor to create.
+     */
+    create: async (
+      monitor: MonitorCreate
+    ): Promise<
+      | {
+          success: true;
+          /**
+           * ID of the created Monitor.
+           */
+          data: string;
+        }
+      | {
+          success: false;
+          message: string;
+        }
+    > => {
+      return {
+        success: false,
+        message: "Not implemented",
+      };
+    },
+
+    /**
+     * Update a Monitor by its ID.
+     * @param id - ID of the Monitor.
+     * @param monitor - The fields to update.
+     */
+    update: async (
+      id: string,
+      monitor: MonitorUpdate
+    ): Promise<
+      | {
+          success: true;
+        }
+      | {
+          success: false;
+          message: string;
+        }
+    > => {
+      return {
+        success: false,
+        message: "Not implemented",
+      };
+    },
+
+    /**
+     * Delete a Monitor by its ID.
+     * @param id - ID of the Monitor.
+     */
+    delete: async (
+      id: string
+    ): Promise<
+      | {
+          success: true;
+        }
+      | {
+          success: false;
+          message: string;
+        }
+    > => {
+      return {
+        success: false,
+        message: "Not implemented",
+      };
+    },
+  };
 }
 
 export {
