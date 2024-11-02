100e3f3860767bd589b926ca1e9d552c61c1d6a6
Alexander Neitzel <alexander.neitzel@garmingo.com>
2024-11-02 17:42:18 +0100

    Add unit tests for retrieving specific Incident by ID

--- tests/incidents.test.ts
+++ tests/incidents.test.ts
@@ -4,6 +4,7 @@
  *   Unauthorized use, reproduction, and distribution of this source code is strictly prohibited.
  */
 import { StatusAPI } from "../src";
+import { ERROR_CODE } from "../src/error";
 
 const statusAPI = new StatusAPI(process.env.API_KEY as string);
 
@@ -20,5 +21,37 @@ describe("Get all Incidents", () => {
     const incidents = incidentResponse.data.incidents;
 
     expect(incidents).toBeDefined();
+  });
+});
+
+describe("Get specific Incident", () => {
+  test("Get Incident by ID", async () => {
+    const INCIDENT_ID = process.env.WORKING_INCIDENT_ID as string;
+    const incidentResponse = await statusAPI.incidents.get(INCIDENT_ID);
+
+    expect(incidentResponse.success).toBe(true);
+
+    if (!incidentResponse.success) {
+      return;
+    }
+
+    const incident = incidentResponse.data;
+
+    expect(incident).toBeDefined();
+
+    expect(incident.id).toBe(INCIDENT_ID);
+  });
+
+  test("Get Incident by Invalid ID", async () => {
+    const INCIDENT_ID = "33333338a7221ba8f0ad8f0f";
+    const incidentResponse = await statusAPI.incidents.get(INCIDENT_ID);
+
+    expect(incidentResponse.success).toBe(false);
+
+    if (incidentResponse.success) {
+      return;
+    }
+
+    expect(incidentResponse.errorCode).toBe(ERROR_CODE.NOT_FOUND);
   });
 });
